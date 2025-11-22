01263edf6b5211802bee5c783cfb267b6aeb97dd
Aum Gajjar <129151745+Aum-gajjar@users.noreply.github.com>
2025-11-22 12:51:34 -0500

    Initial project structure and frontend scaffold

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>AdvisorInsight Dashboard</h1>
+      <p>System Initializing...</p>
+    </div>
+  );
+}
+
+export default App;
